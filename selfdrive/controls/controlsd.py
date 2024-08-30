40bb385a0d9d6b1299280c7115a53a539e6ae87d
Jason Young <jyoung8607@gmail.com>
2024-08-30 14:24:43 -0400

    don't add roll directly to curvature

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -735,7 +735,7 @@ class Controls:
     lp = self.sm['liveParameters']
 
     steer_angle_without_offset = math.radians(CS.steeringAngleDeg - lp.angleOffsetDeg)
-    curvature = -self.VM.calc_curvature(steer_angle_without_offset, CS.vEgo, lp.roll)
+    curvature = -self.VM.calc_curvature(steer_angle_without_offset, CS.vEgo, 0.0)
 
     # controlsState
     dat = messaging.new_message('controlsState')
