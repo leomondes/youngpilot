1f9a88e84cb8bda916d0a3c8353749d0e05c77cb
leomonde <gianlucafedele@gmail.com>
2024-11-16 12:32:16 +0100

    fix self.cp_body in interface.py

--- selfdrive/car/fca_giorgio/interface.py
+++ selfdrive/car/fca_giorgio/interface.py
@@ -28,7 +28,7 @@ class CarInterface(CarInterfaceBase):
 
   # returns a car.CarState
   def _update(self, c):
-    ret = self.CS.update(self.cp, self.cp_cam)
+    ret = self.CS.update(self.cp, self.cp_cam, self.cp_body)
 
     events = self.create_common_events(ret, pcm_enable=not self.CS.CP.openpilotLongitudinalControl)
 
