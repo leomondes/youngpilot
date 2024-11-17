c5313bc6ce76670af36ecc17369f3a21261cb71b
leomonde <gianlucafedele@gmail.com>
2024-11-17 22:51:23 +0100

    change to LKA_ACTIVE = 6

--- selfdrive/car/fca_giorgio/fca_giorgiocan.py
+++ selfdrive/car/fca_giorgio/fca_giorgiocan.py
@@ -1,8 +1,7 @@
 def create_steering_control(packer, bus, apply_steer, lkas_enabled):
   values = {
-    "LKA_ACTIVE": lkas_enabled,
     "LKA_TORQUE": apply_steer,
-    "ACC_ENABLED": lkas_enabled,
+    "LKA_ENABLED": lkas_enabled,
   }
 
   return packer.make_can_msg("LKA_COMMAND", bus, values)
@@ -19,8 +18,7 @@ def create_steering_control(packer, bus, apply_steer, lkas_enabled):
 
 def create_lka_hud_2_control(packer, bus, lat_active):
   values = {
-    "NEW_SIGNAL_1": 1 if lat_active else 0,
-    "NEW_SIGNAL_2": 6 if lat_active else 1,
+    "LKA_ACTIVE": 6,
   }
 
   return packer.make_can_msg("LKA_HUD_2", bus, values)
