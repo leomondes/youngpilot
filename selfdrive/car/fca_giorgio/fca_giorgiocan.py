87fe6b9806b76d7d9e7dc18d88730a23c5c54105
leomonde <gianlucafedele@gmail.com>
2024-11-18 01:30:05 +0100

    add details lka_active variable

--- selfdrive/car/fca_giorgio/fca_giorgiocan.py
+++ selfdrive/car/fca_giorgio/fca_giorgiocan.py
@@ -16,7 +16,7 @@ def create_steering_control(packer, bus, apply_steer, lkas_enabled):
 #  return packer.make_can_msg("LKA_HUD_1", bus, values)
 
 
-def create_lka_hud_2_control(packer, bus, lat_active):
+def create_lka_hud_2_control(packer, bus, apply_steer, lkas_enabled):
   values = {
     "LKA_ACTIVE": 6,
     "NEW_SIGNAL_1": 1,
@@ -24,3 +24,10 @@ def create_lka_hud_2_control(packer, bus, lat_active):
   }
 
   return packer.make_can_msg("LKA_HUD_2", bus, values)
+
+# LKA_ACTIVE
+# 6 (0110) = green car with both white lines
+# 10 (1010) o 12 (1100) yellow on left, positive torque to turn right
+# 8 (1000) o 11 (1011) yellow on right, negative torque to turn left
+# "LKA_ACTIVE": 10 if apply_steer > 0 else 6,
+# "LKA_ACTIVE": 8 if apply_steer < 0 else 6,
