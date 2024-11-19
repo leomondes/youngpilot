e3d00b8a6eafc6b05c8d7bab89056011e26bdfb2
leomonde <gianlucafedele@gmail.com>
2024-11-19 17:28:09 +0100

    test checksum fixo 15

--- selfdrive/car/fca_giorgio/fca_giorgiocan.py
+++ selfdrive/car/fca_giorgio/fca_giorgiocan.py
@@ -18,6 +18,7 @@ def create_steering_control(packer, bus, apply_steer, lkas_enabled, frame):
     "LKA_ENABLED": 0,
     "COUNTER": frame % 0x10,
     #"CHECKSUM": crc8([apply_steer.to_bytes(2), int(0x1).to_bytes(2), frame % 0x10])
+    "CHECKSUM": 15,
   }
 
   return packer.make_can_msg("LKA_COMMAND", bus, values)
