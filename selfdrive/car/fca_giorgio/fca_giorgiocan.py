5be88c82c44407b432d7a7d9d1b5c262b3b990d9
leomonde <gianlucafedele@gmail.com>
2024-11-19 17:16:22 +0100

    try int

--- selfdrive/car/fca_giorgio/fca_giorgiocan.py
+++ selfdrive/car/fca_giorgio/fca_giorgiocan.py
@@ -1,6 +1,6 @@
 def crc8(data):
-  crc = bytes(0xFF)
-  poly = bytes(0x1D)
+  crc = 0xFF
+  poly = 0x1D
 
   for byte in data:
     crc = crc ^ byte
@@ -17,7 +17,7 @@ def create_steering_control(packer, bus, apply_steer, lkas_enabled, frame):
     "LKA_TORQUE": apply_steer,
     "LKA_ENABLED": 0,
     "COUNTER": frame % 0x10,
-    "CHECKSUM": crc8([apply_steer.to_bytes(2), int(0x1).to_bytes(2), frame % 0x10])
+    "CHECKSUM": crc8([int(apply_steer.to_bytes(2)), int(0x1).to_bytes(2), int(frame % 0x10)])
   }
 
   return packer.make_can_msg("LKA_COMMAND", bus, values)
