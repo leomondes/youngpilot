0be8481cdbfbf2e789253325ca0bbab805e6564f
leomonde <gianlucafedele@gmail.com>
2024-11-20 19:50:40 +0100

    START point CHECKSUM

--- opendbc/can/common.cc
+++ opendbc/can/common.cc
@@ -253,7 +253,7 @@ unsigned int hkg_can_fd_checksum(uint32_t address, const Signal &sig, const std:
 
 unsigned int fca_giorgio_checksum(uint32_t address, const Signal &sig, const std::vector<uint8_t> &d) {
   // CRC is in the last byte, poly is same as SAE J1850 but uses a different init value and output XOR
-  uint8_t crc = 0xFF;
+  uint8_t crc = 0x00;
 
   for (int i = 0; i < d.size() - 1; i++) {
     crc ^= d[i];
