6af967c1aaa3f45cb04f3f0401fd6d78d8f92853
leomonde <gianlucafedele@gmail.com>
2024-11-20 17:50:52 +0100

    remove manual checksum and fix opendbc checksum

--- selfdrive/car/fca_giorgio/fca_giorgiocan.py
+++ selfdrive/car/fca_giorgio/fca_giorgiocan.py
@@ -1,55 +1,8 @@
-def crc8(combined_bits):
-  crc = 0xFF
-  poly = 0x1D
-  data = combined_bits.to_bytes(3, byteorder='big')
-  
-  for byte in data:
-    crc = crc ^ byte
-    for _ in range(8):
-      if crc & 0x80:
-        crc = ((crc << 1) ^ poly) & 0xFF
-      else:
-        crc = (crc << 1) & 0xFF
-  return crc ^ 0xFF
-
-def fca_compute_checksum(combined_bits):
-  checksum = 0xFF
-  data = combined_bits.to_bytes(3, byteorder='big')
-
-  for j in range(3):
-    shift = 0x80
-    curr = data[j]
-
-    for _ in range(8):
-      bit_sum = curr & shift
-      temp_chk = checksum & 0x80
-
-      if bit_sum != 0:
-        bit_sum = 0x1C
-        if temp_chk != 0:
-          bit_sum = 1
-        checksum = checksum << 1
-        temp_chk = checksum | 1
-        bit_sum ^= temp_chk
-      
-      else:
-        if temp_chk != 0:
-          bit_sum = 0x1D
-        checksum = checksum << 1
-        bit_sum ^= checksum
-      
-      checksum = bit_sum
-      shift = shift >> 1
-
-  return ~checksum & 0xFF
-
 def create_steering_control(packer, bus, apply_steer, lkas_enabled, frame):
   combined_bits = (apply_steer << 13) | (lkas_enabled << 12) | (0 << 4) | frame
   values = {
     "LKA_TORQUE": apply_steer,
     "LKA_ENABLED": lkas_enabled,
-    "COUNTER": frame & 0xF,
-    "CHKSUM": fca_compute_checksum(combined_bits),
   }
 
   return packer.make_can_msg("LKA_COMMAND", bus, values)
