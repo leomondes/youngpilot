8b0d34bde8ce5527c101df4e61261d4f5ce90ff1
leomonde <gianlucafedele@gmail.com>
2024-11-25 12:14:37 +0100

    fix counter

--- panda/board/safety/safety_fca_giorgio.h
+++ panda/board/safety/safety_fca_giorgio.h
@@ -47,11 +47,11 @@ static uint32_t fca_giorgio_get_checksum(const CANPacket_t *to_push) {
 static uint8_t fca_giorgio_get_counter(const CANPacket_t *to_push) {
   int addr = GET_ADDR(to_push);
   if (addr == 0xFA) { //ABS_3 have different counter byte
-    int counter_byte = (GET_BYTE(to_push, 4U) & 0x78) >> 3;
-  } else { 
+    return (uint8_t)(GET_BYTE(to_push, 4U) & 0x78) >> 3;
+  } else {
     int counter_byte = GET_LEN(to_push) - 2U;
+    return (uint8_t)(GET_BYTE(to_push, counter_byte) & 0xFU);
   }
-  return (uint8_t)(GET_BYTE(to_push, counter_byte) & 0xFU);
 }
 
 static uint32_t fca_giorgio_compute_crc(const CANPacket_t *to_push) {
