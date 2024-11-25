71bb8180e064e6519401ecb95ff44b4a45a37dbf
leomonde <gianlucafedele@gmail.com>
2024-11-25 02:07:07 +0100

    Update safety_fca_giorgio.h

--- panda/board/safety/safety_fca_giorgio.h
+++ panda/board/safety/safety_fca_giorgio.h
@@ -106,8 +106,8 @@ static void fca_giorgio_rx_hook(const CANPacket_t *to_push) {
   // TODO: find cruise button message
 
   if ((GET_BUS(to_push) == 0U) && (addr == FCA_GIORGIO_ENGINE_2)) {
-    gas = ((GET_BYTE(to_push, 1) >> 5) | (GET_BYTE(to_push, 0) & 0x1FU << 3));
-    gas_pressed = bool(gas-1024 > 0);
+    gas_pedal = ((GET_BYTE(to_push, 1) >> 5) | (GET_BYTE(to_push, 0) & 0x1FU << 3));
+    gas_pressed = bool(gas_pedal > 0);
   }
 
   // Signal: ABS_3.BRAKE_PEDAL_SWITCH
