df123f71ff2a2bc61b2186598e8fbd14981df5d4
leomonde <gianlucafedele@gmail.com>
2024-11-26 00:39:22 +0100

    Update test_fca_giorgio.py

--- panda/tests/safety/test_fca_giorgio.py
+++ panda/tests/safety/test_fca_giorgio.py
@@ -40,26 +40,26 @@ class TestFcaGiorgio_Safety(common.PandaCarSafetyTest, common.MotorTorqueSteerin
     values = {"WHEEL_SPEED_%s" % s: speed for s in ["FL", "FR", "RL", "RR"]}
     return self.packer.make_can_msg_panda("ABS_1", 0, values)
 
-  def _speed_msg_2(self, speed):
+  def _speed_msg_2(self, speed=None):
     values = {"VEHICLE_SPEED": speed}
     return self.packer.make_can_msg_panda("ABS_6", 0, values)
 
-  def _user_brake_msg(self, brake):
+  def _user_brake_msg(self, brake=None):
     values = {"BRAKE_PEDAL_SWITCH": 1 if brake else 0}
     return self.packer.make_can_msg_panda("ABS_3", 0, values)
 
   def _user_gas_msg(self, gas_pressed=None):
-    values = {"ACCEL_PEDAL_FOOT": gas_pressed}
+    values = {"ACCEL_PEDAL_FOOT": 1 if gas_pressed > 0 else 0}
     return self.packer.make_can_msg_panda("ENGINE_2", 0, values)
 
   def _torque_meas_msg(self, torque=None):
-    values = {"DRIVER_TORQUE": torque}
+    values = {"DRIVER_TORQUE": 10 if torque > 10 else 0}
     return self.packer.make_can_msg_panda("EPS_2", 0, values)
 
   def _torque_cmd_msg(self, torque, steer_req=1):
     values = {"LKA_TORQUE": torque, "LKA_ENABLED": steer_req}
     return self.packer.make_can_msg_panda("LKA_COMMAND", 0, values)
-
+  
   def test_rx_hook(self):
     for count in range(20):
       self.assertTrue(self._rx(self._speed_msg(0)), f"{count=}")
