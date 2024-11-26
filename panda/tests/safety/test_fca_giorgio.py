ded5e33a4eee82753069d1ae6220e15a182bc2b0
leomonde <gianlucafedele@gmail.com>
2024-11-26 12:33:20 +0100

    fix

--- panda/tests/safety/test_fca_giorgio.py
+++ panda/tests/safety/test_fca_giorgio.py
@@ -50,7 +50,7 @@ class TestFcaGiorgio_Safety(common.PandaCarSafetyTest, common.MotorTorqueSteerin
 
   def _user_gas_msg(self, gas_pressed=1):
     values = {"ACCEL_PEDAL_FOOT": 1 if gas_pressed > 0 else 0}
-   return self.packer.make_can_msg_panda("ENGINE_2", 0, values)
+    return self.packer.make_can_msg_panda("ENGINE_2", 0, values)
 
   def _torque_meas_msg(self, torque):
     values = {"DRIVER_TORQUE": torque}
